13c6fb164cf354121a42c8bbd4a3893a23fb337e
Matias <matiaseiglesias@yahoo.com>
2020-07-27 23:41:21 -0300

    acomodo el codigo

--- adc.py
+++ adc.py
@@ -2,13 +2,39 @@ import  numpy as np
 import numpy.polynomial.polynomial as poly
 import matplotlib.pyplot as plt
 import control.matlab as ml
+from scipy import signal
 
+def aumentar_x_veces_20dB(x, numerador):
+    return (10**x)*numerador
 
-#numerador = [1, 0, 0, 0, 0]
-#denominador = [1, 2463, 3.033e6, 2.118e9, 7.89e11]
 numerador = [-1,0, 0]
 denominador = [1, 1785.714286, 3188775.51]
 
+#Transferencia a partir de polos y ceros
+ceros = [-100]
+polos = [-1000, -10000]
+
+
+numerador = poly.polyfromroots(ceros)
+denominador = poly.polyfromroots(polos)
+
+numerador = numerador[::-1]
+denominador = denominador[::-1]
+
+numerador = aumentar_x_veces_20dB(4,numerador)
+
+G = ml.tf(numerador, denominador)
+print(G)
+print("ceros: ")
+print(ml.zero(G))
+print("polos: ")
+print(ml.pole(G))
+ml.damp(G)
+#mod, fase, w = ml.bode(G)
+#plt.show()
+
+"""
+#calcular polos
 roots = np.roots(denominador)
 print("cantidad de raices")
 print(len(roots))
@@ -20,47 +46,42 @@ coeficientes = poly.polyfromroots(roots)
 print("Coeficientes: ")
 print(coeficientes)
 
+#Analisis rapido de la transferencia
 G = ml.tf(numerador, denominador)
 print(G)
 print("ceros: ")
 print(ml.zero(G))
 print("polos: ")
 print(ml.pole(G))
+ml.damp(G)
 
-def invert(t):
-    return 1/t
+#Grafico Bode
+G = ml.tf(numerador, denominador)
+mod, fase, w = ml.bode(G)
+plt.show()
 
-#mod, fase, w = ml.bode(G)
-#wn=ml.damp(G)
-
+#respuesta al escalon
+G = ml.tf(numerador, denominador)
 yout, T = ml.step(G)
-#yout, T = ml.impulse(G)
-
-#x = np.linspace(0, 0.0005, 1000)
-
-#freq = 10000
-#signal = np.sin(2*np.pi*freq*x)
-#plt.plot(x, np.sin(2*np.pi*freq*x))
-
-#plt.xlabel('Time [seg]')
-
-#plt.ylabel('sin(x)')
-
-#plt.axis('tight')
-
-#yout, T, xout = ml.lsim(G, U = signal, T = x)
-
-#plt.show()
-
 plt.plot(T,yout)
 plt.show()
 
-#from scipy import signal
+#respuesta al impulso
+G = ml.tf(numerador, denominador)
+yout, T = ml.impulse(G)
+plt.plot(T,yout)
+plt.show()
+"""
+#respuesta a la cuadrada o al seno
+G = ml.tf(numerador, denominador)
+frecuencia = 500 #en Hz
+periodo = 1/frecuencia #en segundos
+t = np.linspace(0, 3*periodo, 100000, endpoint=False)
 
-#t = np.linspace(0, .01, 500000, endpoint=False)
+sig = signal.square(2 * np.pi * frecuencia * t)
+#sig = np.sin(2*np.pi*frecuencia*t)
 
-#pwm = signal.square(2 * np.pi * 500 * t)
-
-#yout, T, xout = ml.lsim(G, U = pwm, T = t)
-#plt.plot(T,yout)
-#plt.show()
+yout, T, xout = ml.lsim(G, U = sig, T = t)
+plt.plot(T,yout)
+#plt.plot(t, sig)
+plt.show()
